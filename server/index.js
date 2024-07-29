4afd85ff2a4daa50e517382c74abfa84b1eaf0a6
Anex Jerin <anexcruise@gmail.com>
2024-07-29 23:59:50 +0530

    Routes set up

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
-const cors = require('cors');
 
-
+const authRouter = require('./Routes/authRoutes');
 
 dotenv.config();
 const app = express();
@@ -12,10 +12,12 @@ const port = process.env.PORT;
 const MONGO_URI = process.env.MONGO_URI;
 
 // Middlewares
-app.use(cors);
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // Routes
+
+app.use('/api/auth', authRouter);
 
 // Mongo DB connection
 const databaseConnection = async (link) => {
