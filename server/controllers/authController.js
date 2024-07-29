698c2060a9a7312aa490d9fae562c027c1826e98
Anex Jerin <anexcruise@gmail.com>
2024-07-29 14:11:17 +0530

    User Registration controller

---
+++ server/controllers/authController.js
@@ -0,0 +1,34 @@
+const User = require('../models/userModel');
+const createError = require('../utils/appErrors');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+// REGISTER USER
+exports.signUp = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      return next(
+        new createError('User with similar email already exist', 400)
+      );
+    }
+
+    const hashPassword = await bcrypt.hash(req.body.password, 12);
+    const newUser = await User.create({ ...req.body, password: hashPassword });
+
+    const token = jwt.sign(
+      { id: newUser.__id },
+      'itsuptoyoutoUnlockitdontcare',
+      { expiresIn: '90d' }
+    );
+
+    res.status.json({
+      status: 'success',
+      message: 'user regitered successfully',
+      token,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+exports.login = async () => {};
